c38a375510006dca3c2ffcc161f51ba80747b6a4
akilvein <akilvein@gmail.com>
2020-03-16 21:47:59 +0300

    continue

--- GitInternals/stage1/src/gitinternals/Main.kt
+++ GitInternals/stage1/src/gitinternals/Main.kt
@@ -3,9 +3,166 @@ package gitinternals
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.util.zip.Inflater
+import java.lang.IllegalArgumentException
+import java.util.*
 import java.util.zip.InflaterInputStream
+
+
+sealed class GitObject {
+    class Blob(val text: String) : GitObject() {
+        override fun toString(): String = "Blob: $text"
+    }
+
+    class Commit(val tree: String,
+                 val parents: Array<String>,
+                 val author: String,
+                 val committer: String,
+                 val date: String,
+                 val message: String) : GitObject() {
+        override fun toString(): String =
+                """ |tree: $tree
+                    |parents: ${parents.joinToString()}
+                    |author: $author
+                    |committer: $committer
+                    |date: $date
+                    |message: 
+                    |$message
+                """.trimMargin()
+    }
+
+    class Tree() : GitObject() {
+        override fun toString(): String = "Tree: NOT IMPLEMENTED"
+    }
+
+    companion object {
+        fun parseFromFile(filepath: String): GitObject {
+            val fis = FileInputStream(filepath)
+            val iis = InflaterInputStream(fis)
+
+            var actualLength = 0
+
+            fun readOneLine(): String {
+                val bos = ByteArrayOutputStream()
+                while (true) {
+                    val data = iis.read()
+                    if (data >= 0) {
+                        actualLength++
+                    }
+                    if (data > 0 && data != '\n'.toInt()) {
+                        bos.write(data)
+                    }
+                    else {
+                        break
+                    }
+                }
+                return bos.toString(Charsets.UTF_8)
+            }
+
+            fun readRemainingLines(): String {
+                val bos = ByteArrayOutputStream()
+                while (true) {
+                    val data = iis.read()
+                    if (data >= 0) {
+                        actualLength++
+                    }
+                    if (data > 0) {
+                        bos.write(data)
+                    }
+                    else {
+                        break
+                    }
+                }
+                return bos.toString(Charsets.UTF_8)
+            }
+
+            val (type, declaredLength) = readOneLine().split(" ")
+            actualLength = 0
+
+            when (type) {
+                "blob" -> {
+                    val text = readRemainingLines()
+                    require(declaredLength.toInt() == text.length) {
+                        "Declared length ($declaredLength) does not match actual text length(${text.length})"
+                    }
+                    return Blob(text)
+                }
+
+                "commit" -> {
+                    var line = readOneLine().trim()
+                    val (tree, treeHash) = line.split(" ")
+                    require(tree == "tree")
+                    require(treeHash.length == 40)
+
+                    val parentHashes = mutableListOf<String>()
+                    while (true) {
+                        line = readOneLine()
+                        val (parent, parentHash) = line.split(" ")
+                        if (parent == "parent") {
+                            require(parentHash.length == 40)
+                            parentHashes.add(parentHash)
+                        }
+                        else {
+                            break
+                        }
+                    }
+
+                    val (author, authorLine) = line.split(delimiters = *arrayOf(" "), limit = 2)
+                    require(author == "author")
+
+                    val (committer, committerLine) = readOneLine().split(delimiters = *arrayOf(" "), limit = 2)
+                    require(committer == "committer")
+
+                    val message = readRemainingLines() + 1
+
+                    require(actualLength == declaredLength.toInt()) {
+                        "Declared length ($declaredLength) does not match actual read data size ($actualLength)"
+                    }
+
+                    return Commit(treeHash, parentHashes.toTypedArray(), authorLine, committerLine, "TODO", message)
+                }
+
+                "tree" -> {
+                    return Tree()
+                }
+            }
+
+            throw IllegalArgumentException("could not parse object file")
+        }
+    }
+}
+
+
+fun parseObject(filepath: String) {
+    val fis = FileInputStream(filepath)
+    val inStream = InflaterInputStream(fis)
+    var i = -1
+    while (inStream.read().also { i = it } != 0) {
+        //First line
+    }
+
+    //Content data
+
+    //Content data
+    while (inStream.read().also { i = it } != -1) {
+        while (inStream.read().also { i = it } != 0x20) { //0x20 is the space char
+            //Permission bytes
+        }
+
+        //Filename: 0-terminated
+        var filename = ""
+        while (inStream.read().also { i = it } != 0) {
+            filename += i.toChar()
+        }
+
+        //Hash: 20 byte long, can contain any value, the only way
+        // to be sure is to count the bytes
+        var hash: String? = ""
+        for (count in 0..19) {
+            i = inStream.read()
+            hash += Integer.toHexString(i)
+        }
+    }
+}
 
 
 fun printObject(fullFilePath: String) {
@@ -14,17 +171,10 @@ fun printObject(fullFilePath: String) {
     println("**************************************************************")
     println()
 
-    val fis = FileInputStream(fullFilePath)
-    val fos = ByteArrayOutputStream()
-    val iis = InflaterInputStream(fis)
+    val o = GitObject.parseFromFile(fullFilePath)
 
-    var data: Int
-    while (iis.read().also { data = it } != -1) {
-        fos.write(data)
-    }
-    iis.close()
 
-    println(fos.toString(Charsets.UTF_8))
+    println(o.toString())
 }
 
 fun main(args: Array<String>) {
